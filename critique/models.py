4d8f56399c62e9d4be00946db11b59d6ddc56d11
13155a1 <ppy040@gmail.com>
2024-06-03 20:20:04 +0900

    feat: Critique 모델 생성

---
+++ critique/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Critique(models.Mode):
+    title = models.CharField(max_length=200)
+    review = models.TextField()
+    date_watched = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length=6)
+    poster = models.ImageField(blank=True, upload_to='poster_images/')
+
+    def __str__(self):
+        return self.title
