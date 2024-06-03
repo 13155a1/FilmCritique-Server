80b6ef39583411fbf39a85774a716cb5e0121e41
13155a1 <ppy040@gmail.com>
2024-06-03 21:10:38 +0900

    feat: 리뷰 CRUD 구현

--- critique/views.py
+++ critique/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .serializers import CritiqueSerializer
+from rest_framework.viewsets import ModelViewSet
+from .models import Critique
 
-# Create your views here.
+class CritiqueViewset(ModelViewSet):
+    queryset = Critique.objects.all()
+    serializer_class = CritiqueSerializer
