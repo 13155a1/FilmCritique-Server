4c95996a4da668350240a7cada1f24027af2ae76
13155a1 <ppy040@gmail.com>
2024-06-04 09:11:18 +0900

    chore: cors, csrf 설정에 백엔드 배포 주소 추가

--- config/settings.py
+++ config/settings.py
@@ -36,12 +36,16 @@ CORS_ALLOWED_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
+
+    "https://port-0-filmcritique-server-1272llwn08nul.sel5.cloudtype.app",
 ]
 
 # csrf 설정
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
+
+    "https://port-0-filmcritique-server-1272llwn08nul.sel5.cloudtype.app",
 ]
 
 # Application definition
