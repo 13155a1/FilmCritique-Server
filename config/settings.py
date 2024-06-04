245cd465b589acf282f1f7e3deec9cf6493c19e8
13155a1 <ppy040@gmail.com>
2024-06-04 10:45:36 +0900

    fix: csrf 설정

--- config/settings.py
+++ config/settings.py
@@ -36,7 +36,6 @@ CORS_ALLOWED_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 
 # csrf 설정
-# CSRF 관련 추가
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:3000',
     'http://127.0.0.1:8000',
@@ -66,7 +65,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
