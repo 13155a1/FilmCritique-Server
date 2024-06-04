574319ef2e97058820aed54b8c2d37456671bce6
13155a1 <ppy040@gmail.com>
2024-06-04 10:33:57 +0900

    chore: csrf 해제

--- config/settings.py
+++ config/settings.py
@@ -35,14 +35,6 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 
-# csrf 설정
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:8000",
-
-    "https://port-0-filmcritique-server-1272llwn08nul.sel5.cloudtype.app",
-]
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -66,7 +58,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
